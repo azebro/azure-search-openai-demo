023acb156db9a1835fc7e9743044ef0feda06365
Adam Zebrowski <adam@zebrowski.me>
2023-04-20 11:57:00 +0000

    .

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What are the contractual conditions for IT maintenance?",
+        value: "What are the contractual conditions for IT maintenance?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What are conditions for cartel prevention?", value: "What are conditions for cartel prevention?" },
+    { text: "what are the terms of quality in procurement?", value: "what are the terms of quality in procurement?" }
 ];
 
 interface Props {
