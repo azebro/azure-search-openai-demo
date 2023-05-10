a1742bf3fd00d48c6ebdcfbfbffeaeee111814d9
Adam Zebrowski <azebro@hotmail.com>
2023-05-10 19:59:57 +0300

    .

--- app/backend/app.py
+++ app/backend/app.py
@@ -4,7 +4,7 @@ import time
 import logging
 import openai
 from flask import Flask, request, jsonify
-from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential, AzureKeyCredential
 from azure.search.documents import SearchClient
 from approaches.retrievethenread import RetrieveThenReadApproach
 from approaches.readretrieveread import ReadRetrieveReadApproach
@@ -18,8 +18,11 @@ AZURE_STORAGE_CONTAINER = os.environ.get("AZURE_STORAGE_CONTAINER") or "content"
 AZURE_SEARCH_SERVICE = os.environ.get("AZURE_SEARCH_SERVICE") or "gptkb"
 AZURE_SEARCH_INDEX = os.environ.get("AZURE_SEARCH_INDEX") or "gptkbindex"
 AZURE_OPENAI_SERVICE = os.environ.get("AZURE_OPENAI_SERVICE") or "openai-test-adamze"
-AZURE_OPENAI_GPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_GPT_DEPLOYMENT") or "davinci"
-AZURE_OPENAI_CHATGPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_CHATGPT_DEPLOYMENT") or "chat"
+#AZURE_OPENAI_GPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_GPT_DEPLOYMENT") or "davinci"
+#AZURE_OPENAI_CHATGPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_CHATGPT_DEPLOYMENT") or "chat"
+
+AZURE_OPENAI_GPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_GPT_DEPLOYMENT") or "text-davinci-003"
+AZURE_OPENAI_CHATGPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_CHATGPT_DEPLOYMENT") or "gpt-35-turbo"
 
 KB_FIELDS_CONTENT = os.environ.get("KB_FIELDS_CONTENT") or "content"
 KB_FIELDS_CATEGORY = os.environ.get("KB_FIELDS_CATEGORY") or "category"
@@ -34,15 +37,15 @@ azure_credential = DefaultAzureCredential()
 
 # Used by the OpenAI SDK
 openai.api_type = "azure"
-openai.api_base = f"https://{AZURE_OPENAI_SERVICE}.openai.azure.com"
-#openai.api_base = f"https://dbopenai.openai.azure.com/"
+#openai.api_base = f"https://{AZURE_OPENAI_SERVICE}.openai.azure.com"
+openai.api_base = f"https://dbopenai.openai.azure.com/"
 openai.api_version = "2022-12-01"
 
 # Comment these two lines out if using keys, set your API key in the OPENAI_API_KEY environment variable instead
-openai.api_type = "azure_ad"
-openai_token = azure_credential.get_token("https://cognitiveservices.azure.com/.default")
-openai.api_key = openai_token.token
-#openai.api_key = "39162054caf74e53aed4278880c54c1a"
+#openai.api_type = "azure_ad"
+#openai_token = azure_credential.get_token("https://cognitiveservices.azure.com/.default")
+#openai.api_key = openai_token.token
+openai.api_key = "39162054caf74e53aed4278880c54c1a"
 
 # Set up clients for Cognitive Search and Storage
 search_client = SearchClient(
@@ -86,7 +89,7 @@ def content_file(path):
     
 @app.route("/ask", methods=["POST"])
 def ask():
-    ensure_openai_token()
+    ensure_openai_token(False) #remove False line if not using keys
     approach = request.json["approach"]
     try:
         impl = ask_approaches.get(approach)
@@ -100,7 +103,7 @@ def ask():
     
 @app.route("/chat", methods=["POST"])
 def chat():
-    ensure_openai_token()
+    ensure_openai_token(False) #remove False line if not using keys
     approach = request.json["approach"]
     try:
         impl = chat_approaches.get(approach)
@@ -112,11 +115,12 @@ def chat():
         logging.exception("Exception in /chat")
         return jsonify({"error": str(e)}), 500
 
-def ensure_openai_token():
-    global openai_token
-    if openai_token.expires_on < int(time.time()) - 60:
-        openai_token = azure_credential.get_token("https://cognitiveservices.azure.com/.default")
-        openai.api_key = openai_token.token
+def ensure_openai_token(useToken=True):
+    if useToken:
+        global openai_token
+        if openai_token.expires_on < int(time.time()) - 60:
+            openai_token = azure_credential.get_token("https://cognitiveservices.azure.com/.default")
+            openai.api_key = openai_token.token
     
 if __name__ == "__main__":
     app.run()
